1659e6c3b8ab750467cd15917337b1a64bde5c11
YouCheng Lin <youchenglin0304@gmail.com>
2023-10-07 11:32:35 +0800

    Add ZeroJudge a003.py

---
+++ ZeroJudge/Basic Question Bank/A/a003.py
@@ -0,0 +1,16 @@
+"""
+Problem: https://zerojudge.tw/ShowProblem?problemid=a003
+Title: 兩光法師占卜術
+Time: 2023-10-07 11:30:51 Add
+"""
+
+M, D = map(int, input().split())
+
+S = (M * 2 + D) % 3
+
+if S == 0:
+    print("普通")
+elif S == 1:
+    print("吉")
+else:
+    print("大吉")
