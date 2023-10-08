0201ba138ec44353a5cbf36d4a898e9ddfbfddaa
YouCheng Lin <youchenglin0304@gmail.com>
2023-10-08 11:33:33 +0800

    Add ZeroJudge a034.py

---
+++ ZeroJudge/Basic Question Bank/A/a034.py
@@ -0,0 +1,11 @@
+"""
+Problem: https://zerojudge.tw/ShowProblem?problemid=a034
+Title: 二進位制轉換
+Time: 2023-10-08 11:31:47 Add
+"""
+
+while True:
+    try:
+        print(f"{int(input()):b}")
+    except EOFError:
+        break
