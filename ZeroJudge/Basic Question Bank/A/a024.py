50f859562d21567d5e79931dc824ae29d99c602b
YouCheng Lin <youchenglin0304@gmail.com>
2023-10-08 11:28:55 +0800

    Add ZeroJudge a024.py

---
+++ ZeroJudge/Basic Question Bank/A/a024.py
@@ -0,0 +1,9 @@
+"""
+Problem: https://zerojudge.tw/ShowProblem?problemid=a024
+Title: 最大公因數(GCD)
+Time: 2023-10-08 11:27:57 Add
+"""
+
+from math import gcd
+
+print(gcd(*map(int, input().split())))
