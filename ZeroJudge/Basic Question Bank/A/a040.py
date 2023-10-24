1e6a16a0bed0fb65e583fc96f4cd2515fc6842e0
YouCheng Lin <youchenglin0304@gmail.com>
2023-10-24 14:55:26 +0800

    Fix ZeroJudge a040.py

--- ZeroJudge/Basic Question Bank/A/a040.py
+++ ZeroJudge/Basic Question Bank/A/a040.py
@@ -3,11 +3,14 @@ Problem: https://zerojudge.tw/ShowProblem?problemid=a040
 Title: 阿姆斯壯數
 Time: 2023-10-08 14:51:17 Add
                           Docs: 新增TODO
+      2023-10-24 14:56:03 Fix: 修正迴圈會捨棄輸入的最後一個數字的問題
 """
+
+n, m = map(int, input().split())
 
 find = False
 
-for i in range(*map(int, input().split())):
+for i in range(n, m + 1):
     if sum(map(lambda x: int(x) ** len(str(i)), str(i))) == i:
         print(i, end=" ")
 
