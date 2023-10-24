79918011cd56628332da025f5ff83b66e7d59e21
YouCheng Lin <youchenglin0304@gmail.com>
2023-10-24 20:43:45 +0800

    Refactor ZeroJudge a022.py

--- ZeroJudge/Basic Question Bank/A/a022.py
+++ ZeroJudge/Basic Question Bank/A/a022.py
@@ -2,11 +2,9 @@
 Problem: https://zerojudge.tw/ShowProblem?problemid=a022
 Title: 迴文
 Time: 2023-10-08 11:18:25 Add
+      2023-10-24 20:43:00 Refactor
 """
 
 str = input()
 
-if str == str[::-1]:
-    print("yes")
-else:
-    print("no")
+print("yes" if str == str[::-1] else "no")
