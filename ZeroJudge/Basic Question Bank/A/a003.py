aca74dc673fabe55ce812df09172adf8689a16d9
YouCheng Lin <youchenglin0304@gmail.com>
2023-10-25 19:29:05 +0800

    Refactor ZeroJudge a003.py

--- ZeroJudge/Basic Question Bank/A/a003.py
+++ ZeroJudge/Basic Question Bank/A/a003.py
@@ -2,15 +2,11 @@
 Problem: https://zerojudge.tw/ShowProblem?problemid=a003
 Title: 兩光法師占卜術
 Time: 2023-10-07 11:30:51 Add
+      2023-10-25 19:26:28 Refactor
 """
+
+dic = ["普通", "吉", "大吉"]
 
 M, D = map(int, input().split())
 
-S = (M * 2 + D) % 3
-
-if S == 0:
-    print("普通")
-elif S == 1:
-    print("吉")
-else:
-    print("大吉")
+print(dic[(M * 2 + D) % 3])
