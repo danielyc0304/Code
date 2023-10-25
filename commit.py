de19cfc01c9cb92851183d1d7113b386957727f4
YouCheng Lin <youchenglin0304@gmail.com>
2023-10-25 19:27:51 +0800

    Feat commit.py

--- commit.py
+++ commit.py
@@ -3,22 +3,25 @@ import sys
 
 ROOT = f"{os.getcwd()}/ZeroJudge/Basic Question Bank"
 
-# commit
-file = input("Commit file: ")
+active = input("Active (e.g. c-): ")
 
-if file != "none":
+# commit
+if active[0] == "c":
+    file = input("Commit file (e.g. a001): ")
+    message = input('Commit message (e.g. Add): ')
+
     os.chdir(f"{ROOT}/{file[0].upper()}")
     if os.system(f"git add {file}.py") != 0:
         sys.exit()
-    if os.system(f'git commit -m "Add ZeroJudge {file}.py"') != 0:
+    if os.system(f'git commit -m "{message} ZeroJudge {file}.py"') != 0:
         sys.exit()
     if os.system("git push") != 0:
         sys.exit()
 
 # create
-file = input("Create file: ")
+if active[1] == "c":
+    file = input("Create file (e.g. a001): ")
 
-if file != "none":
     if os.path.isdir(f"{ROOT}/{file[0].upper()}") is False:
         os.mkdir(f"{ROOT}/{file[0].upper()}")
     os.chdir(f"{ROOT}/{file[0].upper()}")
