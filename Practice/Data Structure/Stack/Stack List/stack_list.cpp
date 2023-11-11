72e7b7853dce771d26d4ec2fcf7478f529e4eca2
YouCheng Lin <youchenglin0304@gmail.com>
2023-11-11 19:33:16 +0800

    Add Practice stack_list.cpp

---
+++ Practice/Data Structure/Stack/Stack List/stack_list.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+using namespace std;
+
+class StackList;
+class StackNode {
+  private:
+    int num;
+    StackNode *next;
+
+  public:
+    StackNode(int num) {
+        this->num = num;
+        this->next = nullptr;
+    }
+
+    friend class StackList;
+};
+class StackList {
+  private:
+    int size;
+    StackNode *top;
+
+  public:
+    StackList() {
+        this->size = 0;
+        this->top = nullptr;
+    }
+
+    bool IsEmpty() { return (size == 0); }
+
+    void Top() {
+        if (IsEmpty() == true) {
+            printf("Stack is empty.\n");
+        } else {
+            printf("%d\n", top->num);
+        }
+    }
+
+    void GetSize() { printf("%d\n", size); }
+
+    void Push(int num) {
+        StackNode *new_node = new StackNode(num);
+
+        if (IsEmpty() == false) {
+            new_node->next = top;
+        }
+        top = new_node;
+        size++;
+    }
+
+    void Pop() {
+        if (IsEmpty() == true) {
+            printf("Stack is empty.\n");
+        } else {
+            StackNode *current = top;
+
+            top = top->next;
+            delete current;
+            size--;
+        }
+    }
+};
+
+int main() {
+    StackList stack;
+    string action;
+
+    while (printf("Enter action: ") && getline(cin, action)) {
+        if (action == "is empty") {
+            printf("%s\n", (stack.IsEmpty() == true) ? "true" : "false");
+        } else if (action == "top") {
+            stack.Top();
+        } else if (action == "get size") {
+            stack.GetSize();
+        } else if (action == "push") {
+            int num;
+
+            printf("Enter number: "), scanf("%d", &num), cin.get();
+            stack.Push(num);
+        } else if (action == "pop") {
+            stack.Pop();
+        } else if (action == "exit") {
+            break;
+        } else {
+            printf("Invalid action.\n");
+        }
+    }
+
+    return 0;
+}
