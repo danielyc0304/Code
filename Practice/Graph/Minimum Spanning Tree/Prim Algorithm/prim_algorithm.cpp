3f411a998b3f88a5e3196c920e78f958733d29b6
YouCheng Lin <youchenglin0304@gmail.com>
2024-01-15 00:58:21 +0800

    Style Practice prim_algorithm.cpp

--- Practice/Graph/Minimum Spanning Tree/Prim Algorithm/prim_algorithm.cpp
+++ Practice/Graph/Minimum Spanning Tree/Prim Algorithm/prim_algorithm.cpp
@@ -24,9 +24,9 @@ class MinimumSpanningTree {
   public:
     MinimumSpanningTree(int num_vertex) {
         this->num_vertex = num_vertex;
-        adjacency_matrix.resize(num_vertex);
+        this->adjacency_matrix.resize(num_vertex);
         for (int i = 0; i < num_vertex; i++) {
-            adjacency_matrix[i].resize(num_vertex);
+            this->adjacency_matrix[i].resize(num_vertex);
         }
     }
 
