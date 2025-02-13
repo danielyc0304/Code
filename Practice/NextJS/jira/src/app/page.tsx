b8c9dec2b29f1ee43c838a46254982afb341222f
YouCheng Lin <youchenglin0304@gmail.com>
2025-02-14 02:45:39 +0800

    Add Practice Jira Customizing components

--- Practice/NextJS/jira/src/app/page.tsx
+++ Practice/NextJS/jira/src/app/page.tsx
@@ -1,9 +1,24 @@
-import { TestComponent } from "@/features/test";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
 export default function Home() {
   return (
     <div>
-      <TestComponent></TestComponent>
+      <Input />
+
+      <Button size="xs">Primary</Button>
+
+      <Button variant="secondary">Secondary</Button>
+
+      <Button variant="destructive">Destructive</Button>
+
+      <Button variant="ghost">Ghost</Button>
+
+      <Button variant="muted">Muted</Button>
+
+      <Button variant="outline">Outline</Button>
+
+      <Button variant="teritary">Teritary</Button>
     </div>
   );
 }
