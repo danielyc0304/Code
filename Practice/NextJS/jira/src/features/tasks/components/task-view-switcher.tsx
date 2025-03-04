8c44255e984e22e19630f30c8860dfaebaf1e1d4
YouCheng Lin <youchenglin0304@gmail.com>
2025-03-04 18:05:34 +0800

    Add Practice Jira Building data filters

--- Practice/NextJS/jira/src/features/tasks/components/task-view-switcher.tsx
+++ Practice/NextJS/jira/src/features/tasks/components/task-view-switcher.tsx
@@ -1,18 +1,39 @@
 "use client";
 
-import { PlusIcon } from "lucide-react";
+import { Loader, PlusIcon } from "lucide-react";
+import { useQueryState } from "nuqs";
 
 import { DottedSeparator } from "@/components/dotted-separator";
 import { Button } from "@/components/ui/button";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { DataFilters } from "./data-filters";
 
+import { useWorkspaceId } from "@/features/workspaces/hooks/use-workspace-id";
+import { useGetTasks } from "../api/use-get-tasks";
 import { useCreateTaskModal } from "../hooks/use-create-task-modal";
+import { useTaskFilters } from "../hooks/use-task-filters";
 
 export const TaskViewSwitcher = () => {
+  const [view, setView] = useQueryState("task-view", { defaultValue: "table" });
+
+  const workspaceId = useWorkspaceId();
+
   const { open } = useCreateTaskModal();
+  const [{ status, assigneeId, projectId, dueDate }] = useTaskFilters();
+  const { data: tasks, isLoading: isLoadingTasks } = useGetTasks({
+    workspaceId,
+    projectId,
+    assigneeId,
+    status,
+    dueDate,
+  });
 
   return (
-    <Tabs className="w-full flex-1 rounded-lg border">
+    <Tabs
+      defaultValue={view}
+      onValueChange={setView}
+      className="w-full flex-1 rounded-lg border"
+    >
       <div className="flex h-full flex-col overflow-auto p-4">
         <div className="flex flex-col items-center justify-between gap-y-2 lg:flex-row">
           <TabsList className="w-full lg:w-auto">
@@ -34,22 +55,32 @@ export const TaskViewSwitcher = () => {
             New
           </Button>
         </div>
-        <DottedSeparator className="my-4" />
-        Data filters
-        <DottedSeparator className="my-4" />
-        <>
-          <TabsContent value="table" className="mt-0">
-            Data table
-          </TabsContent>
 
-          <TabsContent value="kanban" className="mt-0">
-            Data kanban
-          </TabsContent>
+        <DottedSeparator className="my-4" />
 
-          <TabsContent value="calendar" className="mt-0">
-            Data calendar
-          </TabsContent>
-        </>
+        <DataFilters />
+
+        <DottedSeparator className="my-4" />
+
+        {isLoadingTasks ? (
+          <div className="flex h-[200px] w-full flex-col items-center justify-center rounded-lg border">
+            <Loader className="size-5 animate-spin text-muted-foreground" />
+          </div>
+        ) : (
+          <>
+            <TabsContent value="table" className="mt-0">
+              {JSON.stringify(tasks)}
+            </TabsContent>
+
+            <TabsContent value="kanban" className="mt-0">
+              {JSON.stringify(tasks)}
+            </TabsContent>
+
+            <TabsContent value="calendar" className="mt-0">
+              {JSON.stringify(tasks)}
+            </TabsContent>
+          </>
+        )}
       </div>
     </Tabs>
   );
