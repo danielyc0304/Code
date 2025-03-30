69199ef7086902f96ea91f1444841184bed5af5a
YouCheng Lin <youchenglin0304@gmail.com>
2025-03-31 02:22:16 +0800

    Add Practice Docs Margin ruler component

--- Practice/NextJS/docs/src/app/documents/[documentId]/editor.tsx
+++ Practice/NextJS/docs/src/app/documents/[documentId]/editor.tsx
@@ -18,6 +18,8 @@ import { EditorContent, useEditor } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import ImageResize from "tiptap-extension-resize-image";
 
+import { Ruler } from "./ruler";
+
 import { useEditorStore } from "@/store/use-editor-store";
 
 import { FontSizeExtension } from "@/extensions/font-size";
@@ -27,6 +29,7 @@ export const Editor = () => {
   const { setEditor } = useEditorStore();
 
   const editor = useEditor({
+    immediatelyRender: false,
     onCreate({ editor }) {
       setEditor(editor);
     },
@@ -107,6 +110,8 @@ export const Editor = () => {
 
   return (
     <div className="size-full overflow-x-auto bg-[#f9fbfd] px-4 print:overflow-visible print:bg-white print:p-0">
+      <Ruler />
+
       <div className="mx-auto flex w-[816px] min-w-max justify-center py-4 print:w-full print:min-w-0 print:py-0">
         <EditorContent editor={editor} />
       </div>
