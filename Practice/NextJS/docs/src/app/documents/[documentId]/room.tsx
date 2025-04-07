55b31c11a5ef4861465d29e21df45980daab960f
YouCheng Lin <youchenglin0304@gmail.com>
2025-04-07 19:34:21 +0800

    Add Practice Docs Room permissions

--- Practice/NextJS/docs/src/app/documents/[documentId]/room.tsx
+++ Practice/NextJS/docs/src/app/documents/[documentId]/room.tsx
@@ -12,11 +12,7 @@ export function Room({ children }: { children: ReactNode }) {
   const params = useParams();
 
   return (
-    <LiveblocksProvider
-      publicApiKey={
-        "pk_dev_vMp36Y6VPkDNe2yuKyDkinW6Wqz-RsK4DjsYo35XRCmjuM2zy4OdWUtAmzHZ4dbc"
-      }
-    >
+    <LiveblocksProvider throttle={16} authEndpoint="/api/liveblocks-auth">
       <RoomProvider id={params.documentId as string}>
         <ClientSideSuspense fallback={<div>Loading…</div>}>
           {children}
